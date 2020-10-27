3b83afaa0ef1f957bb847ad7f9130232c07a364a
devrishav <rishavchakraborty94@gmail.com>
2020-10-27 15:54:54 +0530

    RMX1992: Dont handle security patch through init

--- init/x2.cpp
+++ init/x2.cpp
@@ -26,7 +26,6 @@ struct x2_props
     std::string build_fingerprint;
     std::string device_build;
     std::string product_device;
-    std::string device_spl;
 };
 
 std::vector<std::string> ro_props_default_source_order = {
@@ -97,7 +96,6 @@ void setRMX(const unsigned int variant)
         "unknown-user 10 QKQ1.191201.002 eng.root.20200902.174625 release-keys",
         "RMX1991",
         "RMX1991CN",
-        "2020-08-05",
     };
 
     //RMX992
@@ -106,7 +104,6 @@ void setRMX(const unsigned int variant)
         "unknown-user 10 QKQ1.191201.002 eng.root.20200723.192038 release-keys",
         "RMX1992",
         "RMX1992L1",
-        "2020-07-05",
     };
 
     //RMX1993
@@ -115,7 +112,6 @@ void setRMX(const unsigned int variant)
         "unknown-user 10 QKQ1.191201.002 eng.root.20200820.184008 release-keys",
         "RMX1993",
         "RMX1993L1",
-        "2020-08-05",
     };
 
     const auto set_ro_build_prop = [](const std::string &source,
@@ -140,8 +136,6 @@ void setRMX(const unsigned int variant)
         set_ro_product_prop(source, "name", prop[variant].device_build.c_str());
     }
 
-    // All 3 variants get upadates at different time so let's handle it here.
-    property_override("ro.build.version.security_patch", prop[variant].device_spl.c_str());
 
     // RMX1993 has different ptoduct name due to oversea variants further being divided into spain and europe
     if (variant == 2)
